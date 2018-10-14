1c00a570dac19b4fa43cdf69e3da635629ba9e2e
seckawijoki@gmail.com <seckawijoki@gmail.com>
2018-10-14 16:36:50 +0800

    2018/10/14 周日 16:36:50.12

---
+++ pass_variable_parameters.lua
@@ -0,0 +1,15 @@
+function printParameters(a, b, c, d, e)
+    print(a,b,c,d,e);
+end
+
+printParameters("a")
+printParameters("a","b")
+printParameters("a","b","c")
+printParameters("a","b","c","d")
+printParameters("a","b","c","d","e")
+
+t_scheme = {}
+t_scheme.type = "standalone_game"
+
+print(t_scheme.type)
+print(t_scheme.t)
