a2123fb226871f7c85c44dc22098a76c157a5938
seckawijoki@gmail.com <seckawijoki@gmail.com>
2018-10-20 23:39:30 +0800

    2018/10/20 周六 23:39:30.11

--- 13.1算术运算的Metamethods.lua
+++ 13.1算术运算的Metamethods.lua
@@ -1,5 +1,50 @@
 Set = {}
 
-function Set.new(t)
+Set.mt = {}
 
+function Set.new(t)
+    local set = {}
+    setmetatable(set, Set.mt)
+    for _, l in ipairs(t) do set[l] = true end
+    return set
 end
+
+function Set.union(a, b)
+    local res = Set.new{}
+    for k in pairs(a) do res[k] = true end
+    for k in pairs(b) do res[k] = true end
+    return res
+end
+
+function Set.intersection(a, b)
+    local res = Set.new{}
+    for k in pairs(a) do 
+        res[k] = b[k]
+    end
+    return res
+end
+
+function Set.tostring(set)
+    local left = "{"
+    local sep = ""
+    for element in pairs(set) do 
+        left = left .. sep .. element
+        sep = ", "
+    end
+    return left .. "}"
+end
+
+function Set.print(s)
+    print(Set.tostring(s))
+end
+
+Set.mt.__add = Set.union
+Set.mt.__mul = Set.intersection
+
+s1 = Set.new{2, 10, 20, 30, 50}
+s2 = Set.new{30, 1}
+
+print(getmetatable(s1))
+print(getmetatable(s2))
+
+Set.print((s1 + s2) * s1)
