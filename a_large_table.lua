c71a8597f634545be83b7afe8c1f53992b14e819
seckawijoki@gmail.com <seckawijoki@gmail.com>
2019-01-06 00:33:09 +0800

    2019/01/06 周日  0:33:08.41

---
+++ a_large_table.lua
@@ -0,0 +1,269 @@
+local tSeatInfo = {
+    ts = 1541489645, 
+    name = [[点击编辑]], 
+    skin = {
+        {
+            cfg = {
+                Uin = 1000, 
+                ExchangeRate = 0, 
+                AstringID = 0, 
+                RGBA = [[{{0,222,41,41},{1,247,242,242},{2,231,181,99}}]], 
+                ModelID = 97, 
+                Day = 0, 
+                UseItemID = 0, 
+                BeginTime = 1544716800, 
+                CostType = 2, 
+                Num = 60, 
+                CloseEnv = [[-1]], 
+                EndTime = 1546790400, 
+                Part = 1, 
+                Name = [[15091]], 
+                Desc = [[9246]], 
+                BuyTips = [[]], 
+                CltVersion = [[0.30.12]], 
+                ItemID = 0, 
+                Sex = 1, 
+                Default = 0, 
+                ShieldID = [[]], 
+                photo = [[avatar_icon1000_97]], 
+                Tag = 1
+            }, 
+            skin = {
+                Uin = 204914649, 
+                ExpireTime = -1, 
+                Part = 1, 
+                AuthorUin = 1000, 
+                ModelID = 97, 
+                CreateTime = 1541489643, 
+                ModifyTime = 1541489643, 
+                seq = [[204914649.1000.1.97]], 
+                Tag = 1
+            }
+        }, 
+        {
+            cfg = {
+                Uin = 1000, 
+                ExchangeRate = 0, 
+                AstringID = 0, 
+                RGBA = [[]], 
+                ModelID = 98, 
+                Day = 0, 
+                UseItemID = 0, 
+                BeginTime = 1544716800, 
+                CostType = 2, 
+                Num = 40, 
+                CloseEnv = [[-1]], 
+                EndTime = 1546790400, 
+                Part = 2, 
+                Name = [[15092]], 
+                Desc = [[9246]], 
+                BuyTips = [[]], 
+                CltVersion = [[0.30.12]], 
+                ItemID = 0, 
+                Sex = 1, 
+                Default = 0, 
+                ShieldID = [[]], 
+                photo = [[avatar_icon1000_98]], 
+                Tag = 1
+            }, 
+            skin = {
+                Uin = 204914649, 
+                ExpireTime = -1, 
+                Part = 2, 
+                AuthorUin = 1000, 
+                ModelID = 98, 
+                CreateTime = 1541489643, 
+                ModifyTime = 1541489643, 
+                seq = [[204914649.1000.2.98]], 
+                Tag = 1
+            }
+        }, 
+        [4] = {
+            cfg = {
+                Uin = 1000, 
+                ExchangeRate = 0, 
+                AstringID = 0, 
+                RGBA = [[{{0,222,41,41},{1,247,242,242},{2,82,41,33}}]], 
+                ModelID = 96, 
+                Day = 0, 
+                UseItemID = 0, 
+                BeginTime = 1544716800, 
+                CostType = 2, 
+                Num = 60, 
+                CloseEnv = [[-1]], 
+                EndTime = 1546790400, 
+                Part = 4, 
+                Name = [[15090]], 
+                Desc = [[9246]], 
+                BuyTips = [[]], 
+                CltVersion = [[0.30.12]], 
+                ItemID = 0, 
+                Sex = 1, 
+                Default = 0, 
+                ShieldID = [[]], 
+                photo = [[avatar_icon1000_96]], 
+                Tag = 1
+            }, 
+            skin = {
+                Uin = 204914649, 
+                ExpireTime = -1, 
+                Part = 4, 
+                AuthorUin = 1000, 
+                ModelID = 96, 
+                CreateTime = 1541489644, 
+                ModifyTime = 1541489644, 
+                seq = [[204914649.1000.4.96]], 
+                Tag = 1
+            }
+        }, 
+        [6] = {
+            cfg = {
+                Uin = 1000, 
+                ExchangeRate = 0, 
+                AstringID = 0, 
+                RGBA = [[{{0,222,41,41},{1,247,242,242},{2,82,41,33}}]], 
+                ModelID = 95, 
+                Day = 0, 
+                UseItemID = 0, 
+                BeginTime = 1544716800, 
+                CostType = 2, 
+                Num = 35, 
+                CloseEnv = [[-1]], 
+                EndTime = 1546790400, 
+                Part = 6, 
+                Name = [[15089]], 
+                Desc = [[9246]], 
+                BuyTips = [[]], 
+                CltVersion = [[0.30.12]], 
+                ItemID = 0, 
+                Sex = 1, 
+                Default = 0, 
+                ShieldID = [[]], 
+                photo = [[avatar_icon1000_95]], 
+                Tag = 1
+            }, 
+            skin = {
+                Uin = 204914649, 
+                ExpireTime = -1, 
+                Part = 6, 
+                AuthorUin = 1000, 
+                ModelID = 95, 
+                CreateTime = 1541489644, 
+                ModifyTime = 1541489644, 
+                seq = [[204914649.1000.6.95]], 
+                Tag = 1
+            }
+        }, 
+        [7] = {
+            cfg = {
+                Uin = 1000, 
+                ExchangeRate = 0, 
+                AstringID = 0, 
+                RGBA = [[{{0,222,41,41},{1,247,242,242}}]], 
+                ModelID = 94, 
+                Day = 0, 
+                UseItemID = 0, 
+                BeginTime = 1544716800, 
+                CostType = 2, 
+                Num = 50, 
+                CloseEnv = [[-1]], 
+                EndTime = 1546790400, 
+                Part = 7, 
+                Name = [[15088]], 
+                Desc = [[9246]], 
+                BuyTips = [[]], 
+                CltVersion = [[0.30.12]], 
+                ItemID = 0, 
+                Sex = 1, 
+                Default = 0, 
+                ShieldID = [[]], 
+                photo = [[avatar_icon1000_94]], 
+                Tag = 1
+            }, 
+            skin = {
+                Uin = 204914649, 
+                ExpireTime = -1, 
+                Part = 7, 
+                AuthorUin = 1000, 
+                ModelID = 94, 
+                CreateTime = 1541489645, 
+                ModifyTime = 1541489645, 
+                seq = [[204914649.1000.7.94]], 
+                Tag = 1
+            }
+        }, 
+        [8] = {
+            cfg = {
+                Uin = 1000, 
+                ExchangeRate = 0, 
+                AstringID = 0, 
+                RGBA = [[{{0,222,41,41},{2,247,242,242}}]], 
+                ModelID = 93, 
+                Day = 0, 
+                UseItemID = 0, 
+                BeginTime = 1544716800, 
+                CostType = 2, 
+                Num = 45, 
+                CloseEnv = [[-1]], 
+                EndTime = 1546790400, 
+                Part = 8, 
+                Name = [[15087]], 
+                Desc = [[9246]], 
+                BuyTips = [[]], 
+                CltVersion = [[0.30.12]], 
+                ItemID = 0, 
+                Sex = 2, 
+                Default = 0, 
+                ShieldID = [[]], 
+                photo = [[avatar_icon1000_93]], 
+                Tag = 1
+            }, 
+            skin = {
+                Uin = 204914649, 
+                ExpireTime = -1, 
+                Part = 8, 
+                AuthorUin = 1000, 
+                ModelID = 93, 
+                CreateTime = 1541489645, 
+                ModifyTime = 1541489645, 
+                seq = [[204914649.1000.8.93]], 
+                Tag = 1
+            }
+        }
+    }, 
+    status = [[used]], 
+    seatid = 1, 
+    scale = 1, 
+    texture = [[]]
+}
+
+local function CompressSeatInfoTable(tSeatInfo)
+    print(tostring(tSeatInfo), tostring(tSeatInfo.skin))
+    print(tSeatInfo == nil or tSeatInfo.skin == nil)
+    if tSeatInfo == nil or tSeatInfo.skin == nil then return nil end
+    local tNewSeatInfo = {}
+    local tNewOutterSkin = {}
+    local tOutterSkin = tSeatInfo.skin
+    tNewSeatInfo["skin"] = tNewOutterSkin
+    for k, v in pairs(tOutterSkin) do 
+        local tInnerSkin = v.skin;
+        tNewOutterSkin[k] = {}
+        if tInnerSkin then 
+            local tNewInnerSkin = {}
+            tNewOutterSkin[k]["skin"] = tNewInnerSkin
+            if tInnerSkin.ModelID then
+                tNewInnerSkin["ModelID"] = tInnerSkin.ModelID
+            end
+            if tInnerSkin.Data then
+                tNewInnerSkin["Data"] = tInnerSkin.Data
+            end
+        end
+    end
+    tNewSeatInfo["texture"] = tSeatInfo.texture
+    return tNewSeatInfo
+end
+
+-- printTableRecursively(tSeatInfo)
+print(table.tostring(CompressSeatInfoTable(tSeatInfo)))
+
+-- print(not true or not true)
